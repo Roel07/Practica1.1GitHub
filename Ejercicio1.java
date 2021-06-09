35f61ea642dcc9c95ccec8245b9c73f0acfba3eb
Roel García <brandonescobal201602762.edu.gt>
2021-06-08 21:56:05 -0600

    Ejercicio1 escrito en java

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+//1. Diseñar un algoritmo que, dado dos números, muestre por pantalla su suma
+
+public class Ejercicio1{
+
+
+	public static void main(String[] args) {
+		int numero1 = 45, numero2 = 35;
+
+		int suma = numero1 + numero2;
+
+		System.out.println("La suma de los números es: ") + suma 
+
+	}
+}
