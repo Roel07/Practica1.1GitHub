95debcdacc4ed890dcef8b8cb5f73b904f50d271
Roel García <brandonescobal201602762.edu.gt>
2021-06-09 01:22:49 -0600

    Ejercicio8 Escrito en java

---
+++ Ejercicio8.java
@@ -0,0 +1,13 @@
+/*8. Diseñar un algoritmio que, dados tres números enteros, calcule e imprima el promedio 
+entre ellos*/
+
+public class Ejercicio8{
+
+	public static void main(String[] args) {
+		
+		int numero1 =  85, numero2 = 72, numero3 = 78;
+		double promedio = ((numero1 + numero2 + numero3) / 3);
+
+		System.out.println("El promedio es: "+ promedio);
+	}
+}
